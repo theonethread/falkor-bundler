02d205685b4d1cff1734c7a74473859ddfd49274
Barnabas Bucsy <barnabas.bucsy@gmail.com>
2021-11-07 05:44:34 +0100

    Progress

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import path from "path";
 import minimist from "minimist";
 import shell from "shelljs";
 import stripJsonComments from "strip-json-comments";
+import loggerFactory from "./cli/logger.js";
 
 //#region COMMAND LINE INTERFACE
 
@@ -16,49 +17,34 @@ await (async () => {
     }
 })();
 
+const logger = loggerFactory(argv);
 const arrayOfStringLogJoiner = "', '";
 const cwd = process.cwd();
 
-let printFalkor;
-let printTask;
-let printLog;
-let printWarning;
-let printError;
-if (argv.s || argv.silent) {
-    printFalkor = () => void 0;
-    printTask = () => void 0;
-    printLog = () => void 0;
-    printWarning = () => void 0;
-    printError = () => void 0;
-} else {
-    printFalkor = (...l) => console.log("[Falkor Module Bundler]", ...l);
-    printTask = (...l) => console.log("  #", ...l);
-    printLog = (...l) => console.log("    >", ...l);
-    printWarning = (...l) => console.log("    ! WARNING:", ...l);
-    printError = (...l) => console.error("    ! ERROR:", ...l);
-}
-
 //#endregion
 
-printFalkor("starting");
+logger.printFalkor("starting");
 
 //#region COMMAND LINE VALIDATION
 
-let input = "src/index.ts";
-let outDir = ".dist";
-let jsMode = false;
-let bundleMode = "release";
-let bundleModeSet = false;
-const outerExternals = [];
-const sharedExternals = [];
-const compilationContext = {
-    _DEBUG: false,
-    _RELEASE: true
-};
 // FUTURE: maybe make this configurable?
 const tempTypesDir = ".types";
+const cliConfig = {
+    input: "src/index.ts",
+    outDir: ".dist",
+    jsMode: false,
+    bundleMode: "release",
+    outerExternals: [],
+    sharedExternals: [],
+    compilationContext: {
+        _DEBUG: false,
+        _RELEASE: true
+    }
+};
 
-printTask("validating command line arguments");
+const bundleModeSet = false;
+
+logger.printTask("validating command line arguments");
 
 Object.keys(argv).forEach((arg) => {
     switch (arg) {
@@ -68,7 +54,7 @@ Object.keys(argv).forEach((arg) => {
         case "release":
             const tryingDebug = arg === "d" || arg === "debug";
             if (bundleModeSet || typeof argv[arg] !== "boolean") {
-                printError(
+                logger.printError(
                     `'${tryingDebug ? "debug" : "release"}: -${
                         arg.length > 1 ? "-" : ""
                     }${arg}' must be boolean, or already set (using argument: ${argv[arg]})`
@@ -76,9 +62,9 @@ Object.keys(argv).forEach((arg) => {
                 process.exit(1);
             }
             if (tryingDebug) {
-                compilationContext._DEBUG = argv[arg];
-                compilationContext._RELEASE = !argv[arg];
-                bundleMode = "debug";
+                cliConfig.compilationContext._DEBUG = argv[arg];
+                cliConfig.compilationContext._RELEASE = !argv[arg];
+                cliConfig.bundleMode = "debug";
             }
             bundleModeSet = true;
             break;
@@ -86,27 +72,29 @@ Object.keys(argv).forEach((arg) => {
         case "i":
         case "input":
             if (typeof argv[arg] !== "string") {
-                printError(
+                logger.printError(
                     `'input: -${arg.length > 1 ? "-" : ""}${arg}' must be string (using argument: ${argv[arg]})`
                 );
                 process.exit(1);
             }
-            input = argv[arg];
+            cliConfig.input = argv[arg];
             break;
 
         case "o":
         case "out":
             if (typeof argv[arg] !== "string") {
-                printError(`'out: -${arg.length > 1 ? "-" : ""}${arg}' must be string (using argument: ${argv[arg]})`);
+                logger.printError(
+                    `'out: -${arg.length > 1 ? "-" : ""}${arg}' must be string (using argument: ${argv[arg]})`
+                );
                 process.exit(1);
             }
-            outDir = argv[arg];
+            cliConfig.outDir = argv[arg];
             break;
 
         case "c":
         case "context":
             if (typeof argv[arg] !== "string") {
-                printError(
+                logger.printError(
                     `'context: -${arg.length > 1 ? "-" : ""}${arg}' must be string (using argument: ${argv[arg]})`
                 );
                 process.exit(1);
@@ -122,23 +110,25 @@ Object.keys(argv).forEach((arg) => {
                 ctx = minimist(argvStr.replace(new RegExp("\\" + replacer), "--").split(/\s+/));
                 delete ctx._;
             } catch (e) {
-                printError(`'context: -${arg.length > 1 ? "-" : ""}${arg}' parse error (using argument: ${argv[arg]})`);
+                logger.printError(
+                    `'context: -${arg.length > 1 ? "-" : ""}${arg}' parse error (using argument: ${argv[arg]})`
+                );
                 process.exit(1);
             }
             const excludeContextKeys = ["DEBUG", "RELEASE", "_", "__", "--"];
             Object.keys(ctx).forEach((c) => {
                 if (excludeContextKeys.includes(c)) {
-                    printWarning(`discarding excluded context key '${c}'`);
+                    logger.printWarning(`discarding excluded context key '${c}'`);
                     return;
                 }
-                compilationContext["_" + c] = ctx[c];
+                cliConfig.compilationContext["_" + c] = ctx[c];
             });
             break;
 
         // extra arguments provided by minimist - must be listed in the end following '--' POSIX separator
         case "--":
             if (argv[arg].length) {
-                outerExternals.push(...argv[arg]);
+                cliConfig.outerExternals.push(...argv[arg]);
             }
             break;
 
@@ -147,24 +137,24 @@ Object.keys(argv).forEach((arg) => {
             break;
 
         default:
-            printWarning(`unhandled CLI argument: '-${arg.length > 1 ? "-" : ""}${arg}' (${argv[arg]})`);
+            logger.printWarning(`unhandled CLI argument: '-${arg.length > 1 ? "-" : ""}${arg}' (${argv[arg]})`);
             break;
     }
 });
 
-const { ext: inputExt, dir: inputDir, name: inputName } = path.parse(input);
+const { ext: inputExt, dir: inputDir, name: inputName } = path.parse(cliConfig.input);
 if (inputExt === ".js") {
-    jsMode = true;
+    cliConfig.jsMode = true;
 } else if (inputExt !== ".ts") {
-    printError(`invalid entry point '${path.resolve(input)}'`);
+    logger.printError(`invalid entry point '${path.resolve(cliConfig.input)}'`);
     process.exit(1);
 }
-if (!shell.test("-f", input)) {
-    printError(`entry point '${path.resolve(input)}' not found`);
+if (!shell.test("-f", cliConfig.input)) {
+    logger.printError(`entry point '${path.resolve(cliConfig.input)}' not found`);
     process.exit(1);
 }
 if (shell.test("-e", `${inputDir}/${tempTypesDir}`)) {
-    printError(`output subdirectory '${tempTypesDir}' is reserved, please consider restructuring your sources`);
+    logger.printError(`output subdirectory '${tempTypesDir}' is reserved, please consider restructuring your sources`);
     process.exit(1);
 }
 
@@ -172,7 +162,7 @@ if (shell.test("-e", `${inputDir}/${tempTypesDir}`)) {
 
 //#region TSCONFIG VALIDATION
 
-printTask("validating tsconfig.json");
+logger.printTask("validating tsconfig.json");
 
 const target = "ESNext";
 try {
@@ -180,11 +170,11 @@ try {
     const tsc = JSON.parse(stripJsonComments(shell.cat("tsconfig.json").toString()));
     // FUTURE: extract 'target', support ES5 with package 'browser'?
     if (tsc.target && tsc.target !== target) {
-        printError("invalid 'target' in tsconfig.json, currently only 'ESNext' is supported");
+        logger.printError("invalid 'target' in tsconfig.json, currently only 'ESNext' is supported");
         process.exit(1);
     }
 } catch (e) {
-    printError("invalid tsconfig.json, working directory must be TypeScript project root");
+    logger.printError("invalid tsconfig.json, working directory must be TypeScript project root");
     process.exit(1);
 }
 
@@ -192,7 +182,7 @@ try {
 
 //#region PACKAGE VALIDATION
 
-printTask("validating package.json");
+logger.printTask("validating package.json");
 
 let pkg;
 let moduleName;
@@ -205,28 +195,28 @@ const excludedBinaries = [];
 try {
     pkg = JSON.parse(shell.cat("package.json"));
 } catch (e) {
-    printError("invalid package.json, working directory must be NodeJS module root");
+    logger.printError("invalid package.json, working directory must be NodeJS module root");
     process.exit(1);
 }
 
 if (pkg.type !== "module") {
-    printError("'type' in package.json is not 'module'");
+    logger.printError("'type' in package.json is not 'module'");
     process.exit(1);
 }
-if (pkg.module && pkg.module === `${outDir}/${inputName}.js`) {
-    if (pkg.main !== `${outDir}/${inputName}.js`) {
-        printError(`'main' in package.json is not the same as 'module'`);
+if (pkg.module && pkg.module === `${cliConfig.outDir}/${inputName}.js`) {
+    if (pkg.main !== `${cliConfig.outDir}/${inputName}.js`) {
+        logger.printError(`'main' in package.json is not the same as 'module'`);
         process.exit(1);
     }
-    if (pkg.typings !== `${outDir}/${inputName}.d.ts`) {
-        printError(`'typings' in package.json is not named after 'module'`);
+    if (pkg.typings !== `${cliConfig.outDir}/${inputName}.d.ts`) {
+        logger.printError(`'typings' in package.json is not named after 'module'`);
         process.exit(1);
     }
     libraryMode = true;
     buildModes.push("module");
 }
 // NOTE: replace directory tree with first directory only, since shared modules my not live in the root of the project
-const pathReplacer = new RegExp(`^${outDir.replace(/^(\.\/)?([^\/]+)(.*)?/, "$1$2")}`);
+const pathReplacer = new RegExp(`^${cliConfig.outDir.replace(/^(\.\/)?([^\/]+)(.*)?/, "$1$2")}`);
 const replaceDir = inputDir.replace(/^(\.\/)?([^\/]+)(.*)?/, "$1$2");
 const extReplacer = /\.js$/;
 if (pkg.bin) {
@@ -237,13 +227,13 @@ if (pkg.bin) {
         binaries = Object.entries(pkg.bin);
     }
     for (const [binName, binPath] of binaries) {
-        if (binPath === `${outDir}/${inputName}.js`) {
+        if (binPath === `${cliConfig.outDir}/${inputName}.js`) {
             moduleName = binName;
             binaryMode = true;
             buildModes.push("binary");
         } else {
             let binarySource = binPath.replace(pathReplacer, replaceDir);
-            if (!jsMode) {
+            if (!cliConfig.jsMode) {
                 binarySource = binarySource.replace(extReplacer, ".ts");
             }
             excludedBinaries.push(binarySource);
@@ -258,23 +248,23 @@ if (pkg.shared) {
         sharedModules = pkg.shared;
     }
     sharedModules.forEach((sharedPath) => {
-        if (sharedPath === `${outDir}/${inputName}.js`) {
+        if (sharedPath === `${cliConfig.outDir}/${inputName}.js`) {
             moduleName = inputName;
             sharedMode = true;
             buildModes.push("shared");
         } else {
             let sharedSource = sharedPath.replace(pathReplacer, replaceDir);
-            if (!jsMode) {
-                sharedExternals.push(sharedSource);
+            if (!cliConfig.jsMode) {
+                cliConfig.sharedExternals.push(sharedSource);
                 sharedSource = sharedSource.replace(extReplacer, ".ts");
             }
-            sharedExternals.push(sharedSource);
+            cliConfig.sharedExternals.push(sharedSource);
         }
     });
 }
 
 if (!libraryMode && !binaryMode && !sharedMode) {
-    printError("nor 'binary' nor 'module' nor 'shared' build mode could be resolved from package.json");
+    logger.printError("nor 'binary' nor 'module' nor 'shared' build mode could be resolved from package.json");
     process.exit(1);
 }
 
@@ -282,34 +272,34 @@ if (!libraryMode && !binaryMode && !sharedMode) {
 
 //#region PRINT SETTINGS
 
-printTask("validated settings");
-printLog(`working directory: '${cwd}'`);
-printLog(`input: '${input}'${jsMode ? " (JS mode)" : ""}`);
-printLog(`out directory: '${outDir}'`);
-if (outerExternals.length) {
-    printLog(`externals: '${outerExternals.join(arrayOfStringLogJoiner)}'`);
+logger.printTask("validated settings");
+logger.printLog(`working directory: '${cwd}'`);
+logger.printLog(`input: '${cliConfig.input}'${cliConfig.jsMode ? " (JS mode)" : ""}`);
+logger.printLog(`out directory: '${cliConfig.outDir}'`);
+if (cliConfig.outerExternals.length) {
+    logger.printLog(`externals: '${cliConfig.outerExternals.join(arrayOfStringLogJoiner)}'`);
 }
-if (sharedExternals.length) {
-    printLog(`shared externals: '${sharedExternals.join(arrayOfStringLogJoiner)}'`);
+if (cliConfig.sharedExternals.length) {
+    logger.printLog(`shared externals: '${cliConfig.sharedExternals.join(arrayOfStringLogJoiner)}'`);
 
-    sharedExternals.forEach((e) => outerExternals.push(path.join(cwd, e)));
+    cliConfig.sharedExternals.forEach((e) => cliConfig.outerExternals.push(path.join(cwd, e)));
 }
 if (excludedBinaries.length) {
-    printLog(`excluded binary sources: '${excludedBinaries.join(arrayOfStringLogJoiner)}'`);
+    logger.printLog(`excluded binary sources: '${excludedBinaries.join(arrayOfStringLogJoiner)}'`);
 }
-printLog(`build context: ${JSON.stringify(compilationContext, null, 2).replace(/\n/g, "\n      ")}`);
+logger.printLog(`build context: ${JSON.stringify(cliConfig.compilationContext, null, 2).replace(/\n/g, "\n      ")}`);
 
 //#endregion
 
 //#region BUNDLE CREATION
 
-printTask(
+logger.printTask(
     `bundling ${buildModes.join(" & ")} '${pkg.name}${
         moduleName && pkg.name !== moduleName ? ":" + moduleName : ""
-    }' (${pkg.version}) in [${bundleMode}] mode`
+    }' (${pkg.version}) in [${cliConfig.bundleMode}] mode`
 );
 
-printLog(`importing dependencies`);
+logger.printLog(`importing dependencies`);
 
 const [rollup, externals, jscc, typescript, terser, dts, renameExtensions] = await Promise.all([
     import("rollup").then((m) => m.rollup),
@@ -318,7 +308,7 @@ const [rollup, externals, jscc, typescript, terser, dts, renameExtensions] = awa
     import("@rollup/plugin-typescript").then((m) => m.default),
     import("rollup-plugin-terser").then((m) => m.terser),
     import("rollup-plugin-dts").then((m) => m.default),
-    !jsMode && sharedExternals.length
+    !cliConfig.jsMode && cliConfig.sharedExternals.length
         ? import("@betit/rollup-plugin-rename-extensions").then((m) => m.default.default) // TODO: weird...
         : Promise.resolve()
 ]);
@@ -338,12 +328,14 @@ const typescriptOptions = {
     outDir,
     target,
     rootDir: inputDir,
-    allowJs: jsMode,
+    allowJs: cliConfig.jsMode,
     declaration: libraryMode,
-    declarationDir: libraryMode ? `${outDir}${compilationContext._DEBUG ? "" : "/" + tempTypesDir}` : undefined,
-    declarationMap: libraryMode && compilationContext._DEBUG,
-    preserveConstEnums: compilationContext._DEBUG,
-    sourceMap: compilationContext._DEBUG,
+    declarationDir: libraryMode
+        ? `${cliConfig.outDir}${cliConfig.compilationContext._DEBUG ? "" : "/" + tempTypesDir}`
+        : undefined,
+    declarationMap: libraryMode && cliConfig.compilationContext._DEBUG,
+    preserveConstEnums: cliConfig.compilationContext._DEBUG,
+    sourceMap: cliConfig.compilationContext._DEBUG,
     module: "ESNext",
     alwaysStrict: true,
     removeComments: false,
@@ -358,27 +350,27 @@ const typescriptOptions = {
 };
 
 const jsccOptions = {
-    values: compilationContext,
-    sourcemap: compilationContext._DEBUG,
+    values: cliConfig.compilationContext,
+    sourcemap: cliConfig.compilationContext._DEBUG,
     mapContent: false,
     asloader: false
 };
 
 const outputOptions = {
-    dir: outDir,
+    dir: cliConfig.outDir,
     format: "es",
-    sourcemap: compilationContext._DEBUG,
+    sourcemap: cliConfig.compilationContext._DEBUG,
     sourcemapExcludeSources: true,
     banner: binaryMode ? "#!/usr/bin/env node" : undefined
 };
 
 try {
-    printLog("creating javascript bundle");
+    logger.printLog("creating javascript bundle");
 
     const sourceBundle = await rollup({
-        input,
+        input: cliConfig.input,
         preserveEntrySignatures: libraryMode || sharedMode ? "strict" : false,
-        external: outerExternals.length ? outerExternals : undefined,
+        external: cliConfig.outerExternals.length ? cliConfig.outerExternals : undefined,
         plugins: [
             externals(externalsOptions),
             // since using es modules, ts transpilation must come first
@@ -387,57 +379,57 @@ try {
             // WARNING: do not remove comments in this step, they are needed by JSCC
             typescript(typescriptOptions),
             // TypeScript shared externals' import extension will get rewritten from '.js' to '.ts', we fix that with this plugin in the final output
-            !jsMode && sharedExternals.length
+            !cliConfig.jsMode && cliConfig.sharedExternals.length
                 ? renameExtensions({
                       include: ["**/*.ts"],
                       mappings: { ".ts": ".js" },
-                      sourceMap: compilationContext._DEBUG
+                      sourceMap: cliConfig.compilationContext._DEBUG
                   })
                 : undefined,
             // resolve compile-time conditions in resulting javascript files
             jscc(jsccOptions),
             // minify code in release mode
-            compilationContext._RELEASE ? terser() : undefined
+            cliConfig.compilationContext._RELEASE ? terser() : undefined
         ]
     });
 
-    printLog("writing javascript bundle");
+    logger.printLog("writing javascript bundle");
     await sourceBundle.write(outputOptions);
 
-    printLog("closing javascript bundle");
+    logger.printLog("closing javascript bundle");
     await sourceBundle.close();
 
-    if (libraryMode && compilationContext._RELEASE) {
-        printLog("creating typings bundle");
+    if (libraryMode && cliConfig.compilationContext._RELEASE) {
+        logger.printLog("creating typings bundle");
 
         const typingsOutputOptions = {
-            file: `${outDir}/${inputName}.d.ts`,
-            sourcemap: compilationContext._DEBUG,
+            file: `${cliConfig.outDir}/${inputName}.d.ts`,
+            sourcemap: cliConfig.compilationContext._DEBUG,
             format: "es"
         };
 
         // NOTE: in release mode the typings by tsc get written to a temporary folder '${tempTypesDir}' before flattening them to output directory
         const typingsBundle = await rollup({
-            input: `${outDir}/${tempTypesDir}/${inputName}.d.ts`,
+            input: `${cliConfig.outDir}/${tempTypesDir}/${inputName}.d.ts`,
             plugins: [externals(externalsOptions), dts()]
         });
 
-        printLog("writing typings bundle");
+        logger.printLog("writing typings bundle");
         await typingsBundle.write(typingsOutputOptions);
 
-        printLog("closing typings bundle");
+        logger.printLog("closing typings bundle");
         await typingsBundle.close();
 
-        printLog(`removing intermediate typings files from '${tempTypesDir}'`);
-        shell.rm("-rf", `${outDir}/${tempTypesDir}`);
+        logger.printLog(`removing intermediate typings files from '${tempTypesDir}'`);
+        shell.rm("-rf", `${cliConfig.outDir}/${tempTypesDir}`);
     }
 } catch (e) {
-    printError("unhandled bundling error:");
-    printLog(e);
+    logger.printError("unhandled bundling error:");
+    logger.printLog(e);
     process.exit(1);
 }
 
 //#endregion
 
-printFalkor("finished");
+logger.printFalkor("finished");
 process.exit(0);
