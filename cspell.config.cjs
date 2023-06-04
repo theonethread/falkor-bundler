2cacba4118bb3aabc352d9a808edf9e21aaf146d
Barnabas Bucsy <barnabas.bucsy@gmail.com>
2023-06-04 16:29:24 +0200

    Monorepo progress

---
+++ cspell.config.cjs
@@ -0,0 +1,4 @@
+/** @type { import("cspell").CSpellSettings } */
+const baseConfig = require("@falkor/falkor-cspell-config");
+baseConfig.words.push("asloader", "betit");
+module.exports = baseConfig;
